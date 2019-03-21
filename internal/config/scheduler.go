ba381d17ba11c0b5e3e039546caae731819fedd8
Alexander Sedelnikov <alexander.sedelnikov@gmail.com>
2019-03-22 00:26:04 +0700

    Rename schedule config field.

--- internal/config/scheduler.go
+++ internal/config/scheduler.go
@@ -7,33 +7,33 @@ type SchedulerSettings struct {
 	// DefaultInterval used when task interval == 0
 	DefaultInterval int
 
-	// BucketsCount defines parallel scheduler timers
+	// MaxConcurrency defines parallel scheduler timers
 	// with individual heap.
-	BucketsCount int
+	MaxConcurrency int
 }
 
 // FromViperSchedulerSettings fill-up configuration structure from viper.
 func FromViperSchedulerSettings(v *viper.Viper) *SchedulerSettings {
 	const (
 		kdefaultinterval = "scheduler.defaults.interval"
-		kbucketscount    = "scheduler.bucketscount"
+		kmaxconcurrency    = "scheduler.maxconcurrency"
 	)
 
 	// Setup defaults
 	v.SetDefault(kdefaultinterval, 60000)
-	v.SetDefault(kbucketscount, 16)
+	v.SetDefault(kmaxconcurrency, 16)
 
 	// Validations
 	if v.GetInt(kdefaultinterval) < 1000 {
 		panic(kdefaultinterval + " has too low value")
 	}
 
-	if v.GetInt(kbucketscount) < 1 {
-		panic(kbucketscount + " has too low value")
+	if v.GetInt(kmaxconcurrency) < 1 {
+		panic(kmaxconcurrency + " has too low value")
 	}
 
 	return &SchedulerSettings{
 		DefaultInterval: v.GetInt(kdefaultinterval),
-		BucketsCount:    v.GetInt(kbucketscount),
+		MaxConcurrency:    v.GetInt(kmaxconcurrency),
 	}
 }
